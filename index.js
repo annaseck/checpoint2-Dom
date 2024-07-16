02edbdfd8837112575c84e0cc03b5827eeaefdb4
annaseck <annaseck520@gmail.com>
2024-07-16 10:39:50 +0000

    Ajout de trois fichiers

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const box = document.getElementById("color-box");
+    const changeColor = document.getElementById("change-color-btn");
+ 
+    const getRandomColor = () =>{
+        const random = (Math.random() * 0xFFFFFF << 0).toString(16);
+        // console.log(random);
+        box.style.backgroundColor = "#" + random;
+    }
+
+    changeColor.addEventListener("click" , getRandomColor)
+})
